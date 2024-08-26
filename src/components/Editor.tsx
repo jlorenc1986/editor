39442e975ebffc9d380247b9acca067c3a52a653
Lorenc Estrefi <jlorenc1986@gmail.com>
2024-08-27 00:07:25 +0200

    basic solution

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+import { Converter } from "showdown";
 
 export default function Editor() {
+
+    const [markdown, setMarkdown] = useState("")
+    const [html, setHtml] = useState("")
+
+    function handleRenderMarkdown(){
+        const converter = new Converter();
+        console.log(converter.makeMarkdown(markdown));
+         setHtml(converter.makeHtml(markdown));
+
+    }
     return (
         <div id="editor-wrapper">
-            <textarea id="editor">editor</textarea>
-            <div className="preview"></div>
-            <button className="editor-button"> render markdown</button>
+            <textarea id="editor" cols={30} rows={5} value={markdown} onChange={e => setMarkdown(e.target.value)}></textarea>
+            <div className="preview"  dangerouslySetInnerHTML={{__html: html}} ></div>
+            <button onClick={handleRenderMarkdown}className="editor-button"> Render </button>
         </div>
     )
 }
