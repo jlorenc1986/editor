162a81253adba4a535f9226e0cea38d0c072ec6b
Lorenc Estrefi <jlorenc1986@gmail.com>
2024-08-29 14:16:37 +0200

    proper test for usecase

--- e2e/Editor.spec.ts
+++ e2e/Editor.spec.ts
@@ -1,6 +1,21 @@
 import { test, expect } from '@playwright/test'
+import { markdownHeaderLevelThree } from '../src/constants/test.mocks';
  
+/**
+ * check if app is rendered
+ */
 test('hello world', async ({ page }) => {
   await page.goto('/');
   await expect(page.getByText('Markdown Editor')).toBeVisible();
 });
+
+
+/**
+ * check if user types on editor are the preview changes accordingly 
+ */
+test.only('write markdown and get proper preview', async ({ page }) => {
+  await page.goto('/');
+  await page.locator('#editor').fill(markdownHeaderLevelThree);
+  await expect( (await page.locator('.preview').allInnerTexts()).toLocaleString()).toContain("A third-level heading");
+});
+
